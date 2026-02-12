2f18ae79fcfc9f7f4fe244651eafd342bbc0933c
Nikhil <nikhilgupta542006@gmail.com>
2026-02-12 07:07:32 +0530

    user passord save generate tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userScheema= new mongoose.Schema({
     username:{
@@ -46,4 +49,56 @@ const userScheema= new mongoose.Schema({
 })
 
 
+userScheema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+    this.password=await  bcrypt.hash("password",10)
+     next()
+
+   userScheema.method.isPasswordCorrect=async function (password){
+    return await bcrypt.compare(password,this.password)
+   }  
+})
+
+userScheema.method.generateAccessToken=function async(){
+    return  jwt.sign({
+        _id:this._id,
+      email: this.email,
+            username: this.username,
+            fullName: this.fullName
+    },
+process.env.ACCESS_TOKEN_SECRET),
+{
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+}
+}
+
+userScheema.method.generateRefreshToken=function async(){
+    return jwt.sign({
+        _id:this._id
+    },
+     process.env.REFRESH_TOKEN_SECRET),
+     {
+        expiresIn:REFRESH_TOKEN_EXPIRY
+     }
+}
+
  export const User= mongoose.model("User",userScheema)
+
+
+
+
+
+
+
+
+/*
+todos----
+1- basic struture of import mongoose
+2-create userScheema
+3-export User jo hr file me import hoga 
+4-use middleware if require like to save password in the mongoDB first it convert into hash 
+5- use pre hook with the useScheema its work is to save password in mongodb in hashed formate 
+6-validate the password 
+7-generate Access token and refresh token 
+8-refresh token should be store in the db 
+*/
