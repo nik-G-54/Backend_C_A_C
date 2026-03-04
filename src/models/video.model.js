6c5512944b44015842ae3b1a8a1ea2939986a190
Nikhil <nikhilgupta542006@gmail.com>
2026-03-04 23:16:07 +0530

    streak

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,3 +1,42 @@
+// import mongoose, { Schema } from "mongoose";
+
+// const videoSchema = new Schema({
+//     videoFile: {
+//         type: String, //cloudinary url
+//         required: true
+//     },
+//     thumbnail: {
+//         type: String, //cloudinary url
+//         required: true
+//     },
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     duration: {
+//         type: Number,
+//         required: true
+//     },
+//     views: {
+//         type: Number,
+//         default: 0
+//     },
+//     owner: {
+//         type: Schema.Types.ObjectId,
+//         ref: "User"
+//     },
+//     isPublished: {
+//         type: Boolean,
+//         default: true
+//     },
+// }, { timestamps: true })
+
+
+// export const Video = mongoose.model("Video", videoSchema)
 import mongoose, { Schema } from "mongoose";
 
 const videoSchema = new Schema({
